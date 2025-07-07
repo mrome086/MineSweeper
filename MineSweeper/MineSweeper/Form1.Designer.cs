93b501543e736b1af383b560faa3b7057c9472be
mrome086 <123918945+mrome086@users.noreply.github.com>
2025-07-06 20:05:11 -0400

    Add files via upload

---
+++ MineSweeper/MineSweeper/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace MineSweeper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.easy = new System.Windows.Forms.RadioButton();
+            this.medium = new System.Windows.Forms.RadioButton();
+            this.expert = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(199, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Play);
+            // 
+            // easy
+            // 
+            this.easy.AutoSize = true;
+            this.easy.Location = new System.Drawing.Point(128, 54);
+            this.easy.Name = "easy";
+            this.easy.Size = new System.Drawing.Size(48, 17);
+            this.easy.TabIndex = 1;
+            this.easy.TabStop = true;
+            this.easy.Text = "Easy";
+            this.easy.UseVisualStyleBackColor = true;
+            this.easy.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // medium
+            // 
+            this.medium.AutoSize = true;
+            this.medium.Checked = true;
+            this.medium.Location = new System.Drawing.Point(199, 54);
+            this.medium.Name = "medium";
+            this.medium.Size = new System.Drawing.Size(62, 17);
+            this.medium.TabIndex = 2;
+            this.medium.TabStop = true;
+            this.medium.Text = "Medium";
+            this.medium.UseVisualStyleBackColor = true;
+            this.medium.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // expert
+            // 
+            this.expert.AutoSize = true;
+            this.expert.Location = new System.Drawing.Point(312, 54);
+            this.expert.Name = "expert";
+            this.expert.Size = new System.Drawing.Size(55, 17);
+            this.expert.TabIndex = 3;
+            this.expert.TabStop = true;
+            this.expert.Text = "Expert";
+            this.expert.UseVisualStyleBackColor = true;
+            this.expert.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 179);
+            this.Controls.Add(this.expert);
+            this.Controls.Add(this.medium);
+            this.Controls.Add(this.easy);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.Text = "MineSweeper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton easy;
+        private System.Windows.Forms.RadioButton medium;
+        private System.Windows.Forms.RadioButton expert;
+    }
+}
+
---
+++ MineSweeper/MineSweeper/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MineSweeper
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object send, EventArgs e)
+        {
+
+        }  
+        
+        
+        
+        private void Play(object sender, EventArgs e)
+        {
+            int row=0, col=0,mines = 0;//row*col >=18, mines <= row*col/2
+            String text = "";
+            Form2 f = null;
+            if (easy.Checked)
+            {
+                row = col = 9;
+                mines = 10;
+                text = "Easy";
+            }
+            else if (medium.Checked)
+            {
+                row = col = 16;
+                mines = 40;
+                text = "Medium";
+            }
+            else if (expert.Checked)
+            {
+                row = 16;
+                col = 30;
+                mines = 99;
+                text = "Expert";
+            }
+            else
+                return;
+            int size = Math.Min(30, 1000 / Math.Max(row, col));
+            f = new Form2(text, row, col, size,mines);
+            f.Show();
+
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
